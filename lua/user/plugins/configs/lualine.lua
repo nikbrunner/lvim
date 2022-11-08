ae1857db37163eed7ad3639a4353a7ad73afa0ab
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-11-08 19:43:11 +0100

    lualine: render rel. file path

--- lua/user/plugins/configs/lualine.lua
+++ lua/user/plugins/configs/lualine.lua
@@ -1,2 +1,23 @@
 lvim.builtin.lualine.options.component_separators = { left = "", right = "" }
 lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+
+lvim.builtin.lualine.sections.lualine_c = {
+    {
+        "filename",
+        file_status = true, -- Displays file status (readonly status, modified status)
+        path = 1, -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+            modified = "", -- Text to show when the file is modified.
+            readonly = "", -- Text to show when the file is non-modifiable or readonly.
+            unnamed = "[No Name]", -- Text to show for unnamed buffers.
+        },
+    },
+    {
+        "diagnostics",
+    },
+}
