b74cd67be26f87077f37ec919c90a4d6e8cfb5f0
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-11-09 08:49:33 +0100

    plugs: add todo

--- lua/user/plugins/plugins.lua
+++ lua/user/plugins/plugins.lua
@@ -1,8 +1,14 @@
 lvim.plugins = {
     { "~/Documents/dev/repos/personal/terra-theme/terra.nvim" },
-    { "folke/trouble.nvim", cmd = "TroubleToggle" },
+    -- { "~/Documents/dev/repos/personal/terra-theme/nvim" },
     { "ThePrimeagen/harpoon" },
+    { "b0o/incline.nvim" },
+    { "folke/trouble.nvim", cmd = "TroubleToggle" },
     { "folke/zen-mode.nvim" },
+    {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+    },
     { "uga-rosa/ccc.nvim" },
     { "kazhala/close-buffers.nvim" },
     {
@@ -30,8 +36,5 @@ lvim.plugins = {
         requires = "nvim-treesitter/nvim-treesitter",
         after = "nvim-treesitter",
         run = ":TSInstall query",
-    },
-    {
-        "b0o/incline.nvim",
     },
 }
