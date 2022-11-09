76948d11542db11f9eba895efefb8caf3c62425c
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-11-09 07:44:37 +0100

    incline & lualine: adjust status and winbar setup
    squash

--- lua/user/plugins/configs/incline.lua
+++ lua/user/plugins/configs/incline.lua
@@ -3,22 +3,66 @@ if not present then
     return
 end
 
-incline.setup({
-    render = function(props)
-        local bufname = vim.api.nvim_buf_get_name(props.buf)
-        if bufname == "" then
-            return "[No name]"
-        else
-            return vim.fn.fnamemodify(bufname, ":.")
-        end
-    end,
+---@diagnostic disable-next-line: unused-function, unused-local
+local function get_relative_filename(props)
+    local bufname = vim.api.nvim_buf_get_name(props.buf)
+    if bufname == "" then
+        return "[No name]"
+    else
+        return vim.fn.fnamemodify(bufname, ":.")
+    end
+end
 
-    debounce_threshold = {
-        falling = 50,
-        rising = 10,
-    },
+local function get_diagnostic_label(props)
+    local icons = {
+        Error = "",
+        Warn = "",
+        Info = "",
+        Hint = "",
+    }
+
+    local label = {}
+
+    for severity, icon in pairs(icons) do
+        local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+        if n > 0 then
+            table.insert(label, { icon .. " " .. n .. " ", group = "DiagnosticSign" .. severity })
+        end
+    end
+
+    return label
+end
+
+local function get_relative_filename_with_diagnostics(props)
+    local bufname = vim.api.nvim_buf_get_name(props.buf)
+    local filename = vim.fn.fnamemodify(bufname, ":t")
+    local diagnostics = get_diagnostic_label(props)
+    local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "None"
+    local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
+    local relativeFileName = vim.fn.fnamemodify(bufname, ":.")
+
+    local buffer = {
+        { filetype_icon, guifg = color },
+        { " " },
+        { relativeFileName, gui = modified },
+    }
+
+    if #diagnostics > 0 then
+        table.insert(diagnostics, { "| ", guifg = "grey" })
+    end
+
+    for _, buffer_ in ipairs(buffer) do
+        table.insert(diagnostics, buffer_)
+    end
+
+    return diagnostics
+end
+
+incline.setup({
+    render = get_relative_filename_with_diagnostics,
+    debounce_threshold = { falling = 500, rising = 250 },
     hide = {
-        cursorline = false,
+        cursorline = true,
         focused_win = false,
         only_win = false,
     },
@@ -53,8 +97,8 @@ incline.setup({
         padding = 1,
         padding_char = " ",
         placement = {
-            vertical = "top",
-            horizontal = "left",
+            vertical = "bottom",
+            horizontal = "right",
         },
         -- width = "fit",
         width = "fill",
--- lua/user/plugins/configs/lualine.lua
+++ lua/user/plugins/configs/lualine.lua
@@ -1,2 +1,10 @@
 lvim.builtin.lualine.options.component_separators = { left = "", right = "" }
 lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+
+-- Disable Lualine Diagnostics
+local components = require("lvim.core.lualine.components")
+lvim.builtin.lualine.sections.lualine_x = {
+    components.lsp,
+    components.spaces,
+    components.filetype,
+}
