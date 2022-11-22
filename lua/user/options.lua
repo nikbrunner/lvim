4fe3b05f8013d528b8d58d9df449215d342820e4
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-11-22 10:41:02 +0100

    opts: update

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,5 +1,5 @@
 vim.opt.relativenumber = true
-vim.opt.timeoutlen = 50
+vim.opt.timeoutlen = 100
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 10
 vim.opt.scrolloff = 12
@@ -9,5 +9,5 @@ vim.opt.cursorline = true -- highlight the current line
 -- Lvim
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "terra"
+lvim.colorscheme = "lunar"
 lvim.transparent_window = false
