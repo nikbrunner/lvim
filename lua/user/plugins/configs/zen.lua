65c28908522d6f26fc58ebd85f49273a5756f2f7
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-11-22 10:40:54 +0100

    zen: decrease width

--- lua/user/plugins/configs/zen.lua
+++ lua/user/plugins/configs/zen.lua
@@ -6,7 +6,7 @@ end
 zen.setup({
     window = {
         backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-        width = 200, -- width of the Zen window
+        width = 120, -- width of the Zen window
     },
     plugins = {
         -- disable some global vim options (vim.o...)
