aa664a1b0a286c3b21e848bd9a2ec6bd65c7be06
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-12-14 21:01:15 +0100

    nvimtree: increase width

--- lua/user/plugins/configs/nvim-tree.lua
+++ lua/user/plugins/configs/nvim-tree.lua
@@ -1,4 +1,4 @@
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.nvimtree.setup.view.width = 40
+lvim.builtin.nvimtree.setup.view.width = 50
 lvim.builtin.nvimtree.setup.open_on_setup = true
